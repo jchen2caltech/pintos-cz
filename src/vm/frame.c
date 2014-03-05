ee5b4c54f00d3e40a5ac66ce14b28dea83420769
unknown <jchen2@caltech.edu>
2014-03-04 22:13:26 -0800

    Change frame_table to hash-table - Need to modify code that calls frame though

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -7,13 +7,17 @@
 #include <stdlib.h>
 #include <list.h>
 #include <string.h>
+#include <hash.h>
+#include <bitmap.h>
 
-static struct list frame_table;
+static struct frame_table f_table;
 
 struct frame_table_entry *evict_frame(void);
 
-void frame_table_init(void) {
-    list_init(&frame_table);
+void frame_table_init(int num_frames) {
+    hash_init(&(&f_table)->table, frame_hash_func, frame_less_func, NULL);
+    lock_init(&(&f_table)->lock);
+    (&f_table)->frames = bitmap_create((size_t)num_frames);
 }
 
 
@@ -24,14 +28,30 @@ struct frame_table_entry *obtain_frame(void *virtual_addr) {
     page = palloc_get_page(PAL_USER);
     if (page) {
         newframe = (struct frame_table_entry *)malloc(sizeof(struct frame_table_entry));
+        if (!newframe)
+            PANIC("malloc failure\n");
         newframe->physical_addr = page;
         newframe->owner = thread_current();
         newframe->virtual_addr = virtual_addr;
     }
     else {
-        printf("Run out of frames!\n");
-        exit(-1);
+        PANIC("Run out of frames\n");
     }
-    list_push_back(&frame_table, &newframe->elem);
+    hash_insert(&(&f_table)->table, &newframe->elem);
     return newframe;
+}
+
+void frame_hash_func(struct hash_elem *h, void *aux UNUSED) {
+    struct frame_table_entry *fte = hash_entry(h, struct frame_table_entry, 
+                                               elem);
+    return hash_bytes(&fte->physical_addr, sizeof fte->physical_addr);
+}
+
+void frame_less_func(struct hash_elem *h1, struct hash_elem *h2, 
+                     void *aux UNUSED) {
+    struct frame_table_entry *f1 = hash_entry(h1, struct frame_table_entry,
+                                              elem);
+    struct frame_table_entry *f2 = hash_entry(h2, struct frame_table_entry,
+                                              elem);
+    return (f1->physical_addr < f2->physical_addr);
 }
--- src/vm/frame.h
+++ src/vm/frame.h
@@ -4,16 +4,23 @@
 #include "userprog/syscall.h"
 #include "vm/page.h"
 
+struct frame_table {
+    struct hash table;
+    struct lock lock;
+    struct bitmap *frames;
+};
+
 struct frame_table_entry {
     void *physical_addr;
     void *virtual_addr;
     struct supp_table *spt;
-    struct list_elem elem;
+    struct hash_elem elem;
     struct thread *owner;
 };
 
-void frame_table_init(void);
+void frame_table_init(int num_frames);
 
 struct frame_table_entry *obtain_frame(void *virtual_addr);
+
 
 #endif
